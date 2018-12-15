64f6515cbd9dc2dfad2163213198f29199966077
fiex1414 <fiex1414@easv365.com>
2018-12-15 19:35:45 +0100

    media hover og menu

--- js/script.js
+++ js/script.js
@@ -7,4 +7,24 @@
 );
 
 
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    $('.dropdown-trigger').dropdown({
+      hover: false, // Activate on hover
+      coverTrigger: false,
+    }
+  );
+  } else {
+    $('.dropdown-trigger').dropdown({
+      hover: true,
+      coverTrigger: false,
+    }
+  );
+  }
+}
+
+var x = window.matchMedia("(max-width: 1200px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
 //QUIZ
